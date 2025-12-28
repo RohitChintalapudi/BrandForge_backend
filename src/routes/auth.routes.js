73c249a0a846517fe8600ca6c3c5b4ec387ce662
RohitChintalapudi <rohit.chintalapudi.work@gmail.com>
2025-12-28 06:27:33 +0530

    resolved the error inside auth middlewares

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const authMiddleware = require("../middleware/auth.middleware.js");
 const { register, login, logout } = require("../controllers/auth.controller");
 
 router.post("/register", register);
