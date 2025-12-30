f9f2359cb6a2288b31052992319719d3d5fd5c79
RohitChintalapudi <rohit.chintalapudi.work@gmail.com>
2025-12-30 18:23:45 +0530

    origin changed

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ connectDB();
 app.use(
   cors({
     origin: [
-      "http://localhost:3000", // local dev
+      "http://localhost:5173", // local dev
       "https://brand-forge-frontend.vercel.app", // deployed frontend
     ],
     credentials: true,
